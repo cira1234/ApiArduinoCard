6373018618007a577d52a0e931c581257a882957
cira1234 <aunoford89@gmail.com>
2026-03-12 15:22:59 +0700

    Update app.js

--- api/app.js
+++ api/app.js
@@ -27,7 +27,7 @@ io.on("connection", (socket) => {
   ])
 })
 
-var mysql      = require('mysql');
+var mysql      = require('mysql2');
 var connection = mysql.createConnection({
   host     : 'turntable.proxy.rlwy.net',
   port     : 21360,
